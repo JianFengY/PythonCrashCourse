bcb21562269f9ac688c13a83a52ebad63a711321
杨剑锋 <331387202@qq.com>
2018-01-18 22:14:50 +0800

    chapter3

--- chapter03_introducing_lists/bicycles.py
+++ chapter03_introducing_lists/bicycles.py
@@ -6,6 +6,7 @@ Created on 2018年1月18日
 '''
 
 # 使用方括号[]表示列表，并用逗号分隔元素
+# 列表通常包含多个元素，可以指定表示复数的名称
 bicycles = ['trek', 'cannodale', 'redline', 'specialized']
 print(bicycles[0].title())  # 使用索引访问元素，索引从0开始
 print(bicycles[-1])  # 索引-1表示最后一个元素，-2表示倒数第二个，以此类推
